4557f1ff9ad2e1ec5875bdb29f3cf8cf0c7a09be
Javier Buitrago <jbuitrago2014@fau.edu>
2017-12-29 16:24:51 -0500

    message model is complete

--- models/message.js
+++ models/message.js
@@ -1,1 +1,9 @@
 var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var messageschema = new Schema({
+    content: {type: String, required: true},
+    userid:  {type: Schema.Types.ObjectId}
+});
+
+module.exports = mongoose.model('Message', messageschema);
